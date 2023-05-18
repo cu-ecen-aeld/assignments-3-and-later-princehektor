9171c4e4cf975ffcca8c2ded2adf0195eebc07be
anandmad <anand.madhusoodanan1988@gmail.com>
2023-05-17 18:42:45 -0700

    Code for assignment 3 part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,9 +35,19 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    make defconfig
+    # deep clean the kernel build tree
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+
+    # configure for virt arm dev board
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+
+    # build kernel for booting with qemu
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -47,7 +57,14 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
+# Create rootfs folder and cd into it
+mkdir -p ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+
 # TODO: Create necessary base directories
+mkdir -p bin dev etc home lib64 proc sbin tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log 
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +73,52 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    ln -s /bin/cat busybox
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make distclean
+make defconfig
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/busybox ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+# Program interpreter
+cp /home/anand/arm-cross-compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/usr/lib
+
+# shared libraries
+cp /home/anand/arm-cross-compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp /home/anand/arm-cross-compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+cp /home/anand/arm-cross-compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
 
 # TODO: Make device nodes
+cd ${OUTDIR}/rootfs
+sudo mknod -m 666 dev/null c 1 3  # null device
+sudo mknod -m 666 dev/console c 5 1 # console device
+
 
 # TODO: Clean and build the writer utility
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp /home/anand/Desktop/assignment-2-princehektor/finder-app/writer ${OUTDIR}/rootfs/home
+cp /home/anand/Desktop/assignment-2-princehektor/finder-app/finder.sh ${OUTDIR}/rootfs/home
+cp /home/anand/Desktop/assignment-2-princehektor/finder-app/finder-test.sh ${OUTDIR}/rootfs/home
+cp /home/anand/Desktop/assignment-2-princehektor/finder-app/conf/username.txt ${OUTDIR}/rootfs/home
+cp /home/anand/Desktop/courseera-projects/assignments-3-and-later-princehektor/finder-app/autorun-qemu.sh ${OUTDIR}/rootfs/home
 
 # TODO: Chown the root directory
+sudo chown root ${OUTDIR}/rootfs
 
 # TODO: Create initramfs.cpio.gz
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+
+cd ${OUTDIR}
+gzip -f initramfs.cpio
